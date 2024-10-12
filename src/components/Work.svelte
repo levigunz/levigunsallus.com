7530c047d31aa1a18e601e6b466bf2c965e3a527
Levi Gunsallus <levigunz@gmail.com>
2024-10-12 14:08:01 -0500

    Replace ovalhands with onearlieryears

--- src/components/Work.svelte
+++ src/components/Work.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import questanalytics from '$lib/images/network.jpg';
-    import ovalhands from '$lib/images/ovalhands.jpg';
+    import onearlieryears from '$lib/images/onearlieryears.jpg';
 </script>
 
 <section>
@@ -17,12 +17,11 @@
                 </svg>
             </div>
         </a>
-        <a href="//ovalhands.com" target="_blank">
-            <div class="card" id="ovalhands">
-                <!-- Photo by <a href="https://unsplash.com/@rarchitecture_melbourne?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">R ARCHITECTURE</a> on <a href="https://unsplash.com/photos/brown-and-white-concrete-building-near-green-trees-during-daytime-GGupkreKwxA?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a> -->
-                <img class="cover" loading="lazy" src={ovalhands} alt="Ovalhands" />
-                <h4>Ovalhands</h4>
-                <h2>Housing.</h2>
+        <a href="//onearlieryears.com" target="_blank">
+            <div class="card" id="on-earlier-years">
+                <img class="cover" loading="lazy" src={onearlieryears} alt="On Earlier Years" />
+                <h4>On Earlier Years</h4>
+                <h2>Photos & words.</h2>
                 <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0,0,256,256">
                     <g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(8.53333,8.53333)"><path d="M25.98047,2.99023c-0.03726,0.00118 -0.07443,0.00444 -0.11133,0.00977h-5.86914c-0.36064,-0.0051 -0.69608,0.18438 -0.87789,0.49587c-0.18181,0.3115 -0.18181,0.69676 0,1.00825c0.18181,0.3115 0.51725,0.50097 0.87789,0.49587h3.58594l-10.29297,10.29297c-0.26124,0.25082 -0.36648,0.62327 -0.27512,0.97371c0.09136,0.35044 0.36503,0.62411 0.71547,0.71547c0.35044,0.09136 0.72289,-0.01388 0.97371,-0.27512l10.29297,-10.29297v3.58594c-0.0051,0.36064 0.18438,0.69608 0.49587,0.87789c0.3115,0.18181 0.69676,0.18181 1.00825,0c0.3115,-0.18181 0.50097,-0.51725 0.49587,-0.87789v-5.87305c0.04031,-0.29141 -0.04973,-0.58579 -0.24615,-0.80479c-0.19643,-0.219 -0.47931,-0.34042 -0.77338,-0.33192zM6,7c-1.09306,0 -2,0.90694 -2,2v15c0,1.09306 0.90694,2 2,2h15c1.09306,0 2,-0.90694 2,-2v-10v-2.57812l-2,2v2.57813v8h-15v-15h8h2h0.57813l2,-2h-2.57812h-2z"></path></g></g>
                 </svg>
@@ -31,8 +30,7 @@
         <a href="//scarlok.com" target="_blank">
             <div class="card" id="scarlok">
                 <h4>Scarlok</h4>
-                <h2>Who knows.</h2>
-                <svg fill="#ffffff" viewBox="0 0 24 24" id="recycle" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg" class="icon flat-color" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path id="primary" d="M21.71,19a2.06,2.06,0,0,1-1.77,1h0L16,20a1,1,0,0,1,0-2h0l3.93.05c.07-.07,0-.12,0-.13l-2.37-4.1-.15.43a1,1,0,1,1-1.88-.67l.88-2.47a1,1,0,0,1,1.2-.63l2.73.73a1,1,0,1,1-.52,1.93l-.4-.1,2.25,3.89A2.09,2.09,0,0,1,21.71,19ZM15.58,8.2A1,1,0,0,0,16,6.83L13.83,3.05a2,2,0,0,0-1.77-1h0a2.09,2.09,0,0,0-1.81,1L8,6.94l-.11-.4a1,1,0,0,0-1.22-.71,1,1,0,0,0-.71,1.23l.73,2.73a1,1,0,0,0,1,.74.55.55,0,0,0,.18,0L10.4,10a1,1,0,1,0-.36-2l-.45.08L12,4.06s0-.06.13,0l2.13,3.79A1,1,0,0,0,15.58,8.2Zm-5.52,8.15a1,1,0,0,0-1.52,1.3l.3.35H4.1S4,18,4,17.89l2.21-3.73a1,1,0,0,0-.35-1.37,1,1,0,0,0-1.37.35L2.29,16.88a2,2,0,0,0,0,2.05A2.08,2.08,0,0,0,4.09,20h4.5l-.3.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0l2-2a1,1,0,0,0,0-1.36Z" style="fill: #ffffff;"></path></g></svg>
+                <h2>Everything else.</h2>
                 <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0,0,256,256">
                     <g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(8.53333,8.53333)"><path d="M25.98047,2.99023c-0.03726,0.00118 -0.07443,0.00444 -0.11133,0.00977h-5.86914c-0.36064,-0.0051 -0.69608,0.18438 -0.87789,0.49587c-0.18181,0.3115 -0.18181,0.69676 0,1.00825c0.18181,0.3115 0.51725,0.50097 0.87789,0.49587h3.58594l-10.29297,10.29297c-0.26124,0.25082 -0.36648,0.62327 -0.27512,0.97371c0.09136,0.35044 0.36503,0.62411 0.71547,0.71547c0.35044,0.09136 0.72289,-0.01388 0.97371,-0.27512l10.29297,-10.29297v3.58594c-0.0051,0.36064 0.18438,0.69608 0.49587,0.87789c0.3115,0.18181 0.69676,0.18181 1.00825,0c0.3115,-0.18181 0.50097,-0.51725 0.49587,-0.87789v-5.87305c0.04031,-0.29141 -0.04973,-0.58579 -0.24615,-0.80479c-0.19643,-0.219 -0.47931,-0.34042 -0.77338,-0.33192zM6,7c-1.09306,0 -2,0.90694 -2,2v15c0,1.09306 0.90694,2 2,2h15c1.09306,0 2,-0.90694 2,-2v-10v-2.57812l-2,2v2.57813v8h-15v-15h8h2h0.57813l2,-2h-2.57812h-2z"></path></g></g>
                 </svg>
@@ -95,21 +93,12 @@
         color: white;
     }
 
-    #ovalhands {
-        color: #f1ad02;
+    #on-earlier-years {
+        color: white;
     }
 
     #scarlok {
         background-color: black;
-    }
-
-    svg#recycle {
-        position: absolute;
-        width: 20%;
-        height: 20%;
-        left: 50%;
-        top: 50%;
-        transform: translate(-50%, -50%);
     }
 
     a {
